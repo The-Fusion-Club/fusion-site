c53424ec383b1355a1aebaef45390d03a0b47cb2
Manomay Kagalkar <manomay111@gmail.com>
2023-04-07 15:59:36 +0530

    tailwind setup

--- thefusionclub/src/containers/Home.js
+++ thefusionclub/src/containers/Home.js
@@ -3,10 +3,9 @@ import logo from '../resources/logo.png'
 const Home = () => {
     return(
         <>
-            <header className="App">
-            {/* <img src={disco} className="App-logo" alt="logo" /> */}
-            <img src={logo} className="fusion-logo" alt="fusion logo"></img>
-            {/* <h6>We're building something exciting for you soon!</h6> */}
+            <header className="App flex flex-col w-screen h-screen justify-center items-center gap-5 bg-black text-white">
+                <img src={logo} className="w-8/12" alt="fusion logo"></img>
+                <h6 className='w-8/12 md:text-2xl text-center text-xl font-bold'>We're building something exciting for you soon!</h6>
             </header>
         </>
     )
--- thefusionclub/src/containers/NotFound.js
+++ thefusionclub/src/containers/NotFound.js
@@ -2,25 +2,25 @@ import disco from '../resources/disco.gif'
 
 const NotFound = () => {
     return(
-        <>
-            <main style={notFoundStyle}>
-                <img src={disco} alt='disco ball' style={{ width: '50vw', maxWidth: '400px' }}/>
-                <h1>404 Not Found</h1>
-            </main>
-        </>
+        <main className='flex flex-col w-screen h-screen justify-center items-center gap-10 bg-black text-white'>
+            <div className='flex justify-center'>
+                <img src={disco} alt='disco ball' className='w-6/12 md:w-8/12'/>
+            </div>
+            <h1 className='text-xl md:text-2xl w-8/12 font-bold text-center text-pink-400'>We could not find what you were looking for ¯\_(ツ)_/¯</h1>
+        </main>
     )
 }
 
 export default NotFound
 
-const notFoundStyle = {
-    width: '100vw',
-    minHeight: '100vh',
-    backgroundColor: 'black',
-    color: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '50px',
-}
+// const notFoundStyle = {
+//     width: '100vw',
+//     minHeight: '100vh',
+//     backgroundColor: 'black',
+//     color: 'white',
+//     display: 'flex',
+//     flexDirection: 'column',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     gap: '50px',
+// }
